f0a8a44cfdad523a34b62357f4ca2615f7d59541
skrewde <94148640+skrewde@users.noreply.github.com>
2022-08-14 13:33:41 +0100

    Corrected typo in last line

--- explicit_object_copy.py
+++ explicit_object_copy.py
@@ -11,21 +11,23 @@ class Person:
 person_1 = Person("Peter", "Florida")
 person_2 = Person("Shannon", "Atlanta")
 
-# using deepcopy method to create an explicit copy
+# Using .deepcopy method to create an explicit copy
 person_3 = copy.deepcopy(person_1)
 
-# Test for copy
+# Manipulating copy of person_1
 person_3.name = "Richard"
 
 # This is a test for deepcopy. If deepcopy was unsuccessful, person_1's ID would match person_3's
-print(id(person_1))
-print(id(person_2))
-print(id(person_3))
+def deepcopy_test():
+    print(id(person_1))
+    print(id(person_2))
+    print(id(person_3))
 
 # Another test. If deepcopy was unsuccessful, person_1's attributes would match person_3's
-def deepcopy_test():
+def deepcopy_test2():
     print(person_1.name, person_1.location)
     print(person_2.name, person_2.location)
     print(person_3.name, person_3.location)
 
-deepcopy_test
+deepcopy_test()
+deepcopy_test2()
